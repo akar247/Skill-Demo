8d0d7e88c8aa36505e18560a211836a653ce8e2f
akar247 <akar@ucsd.edu>
2022-04-29 19:02:40 -0700

    Fixed Failing Test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(3, SkillDemo.add(1,1)); //test should fail
+        assertEquals(3, SkillDemo.add(2,1)); //test should now pass
         // 1 + 1 = 2 != 3
     }
 }
